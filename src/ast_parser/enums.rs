51af92dd3be73d8367643cb570b69026779373b2
Bella <annabel.jakob@hotmail.de>
2022-02-03 17:37:37 +0000

    Added unit test for add_lead

---
+++ src/ast_parser/enums.rs
@@ -0,0 +1,12 @@
+pub enum CategoryNodeType {
+    TITLE,
+    CATEGORY,
+    CITATION,
+    INFOBOX_TEMPLATE_NAME
+}
+
+pub enum OpNodeType {
+    NOT,
+    OR,
+    AND
+}
--- src/tests/unit_tests.rs
+++ src/tests/unit_tests.rs
@@ -1,4 +1,13 @@
+use crate::ast_parser::ast_parser::add_leaf;
+
 #[test]
 fn hello_world() {
     assert_eq!(2 + 2, 4);
+}
+
+// AST Parser Tests
+#[test]
+fn test_create_leaf() {
+    let leaf_node = add_leaf("data".to_string());
+    assert_eq!(leaf_node.data,"data");
 }
