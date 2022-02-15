3fee5b0119b4620f1cbfd2ed11bff18dbcac8e4f
makspll <makspl17@gmail.com>
2022-02-15 20:38:01 +0000

    added error handle

--- src/search/search.rs
+++ src/search/search.rs
@@ -3,17 +3,19 @@ use std::sync::RwLock;
 use crate::index::index_structs::Posting;
 use crate::Index;
 use crate::parser::ast::{Query};
+use crate::index::errors::IndexError;
 
-fn execute_query(query: Box<Query>, index: Arc<RwLock<Box<dyn Index>>>) -> Vec<Posting>{
+fn execute_query(query: Box<Query>, index: Arc<RwLock<Box<dyn Index>>>) -> Result<Vec<Posting>, IndexError>{
+    
     match *query{
-        Query::BinaryQuery{op, lhs, rhs} => Vec::default(),
-        Query::UnaryQuery{op, sub} => Vec::default(),
-        Query::PhraseQuery{tks} => Vec::default(),
-        Query::DistanceQuery{dst, lhs, rhs} => Vec::default(),
-        Query::StructureQuery{elem, sub} => Vec::default(),
-        Query::RelationQuery{root, hops, sub} => Vec::default(),
-        Query::WildcardQuery{prefix, postfix} => Vec::default(),
-        Query::FreetextQuery{tokens} => Vec::default(),
+        Query::BinaryQuery{op, lhs, rhs} => Ok(Vec::default()),
+        Query::UnaryQuery{op, sub} => Ok(Vec::default()),
+        Query::PhraseQuery{tks} => Ok(Vec::default()),
+        Query::DistanceQuery{dst, lhs, rhs} => Ok(Vec::default()),
+        Query::StructureQuery{elem, sub} => Ok(Vec::default()),
+        Query::RelationQuery{root, hops, sub} => Ok(Vec::default()),
+        Query::WildcardQuery{prefix, postfix} => Ok(Vec::default()),
+        Query::FreetextQuery{tokens} => Ok(Vec::default()),
     }
 }
 
