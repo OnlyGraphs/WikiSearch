93bc87dcd1cace84a89fdb6595b3d841d7c7c263
makspll <makspl17@gmail.com>
2022-02-18 12:27:09 +0000

    added parser tests, fixed not query not being parsed with `parse_query`

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -90,6 +90,7 @@ pub fn parse_query(nxt: &str) -> IResult<&str, Box<Query>> {
     alt((
         parse_dist_query,
         parse_binary_query,
+        parse_not_query,
         parse_structure_query,
         parse_freetext_query,
     ))(nxt)
